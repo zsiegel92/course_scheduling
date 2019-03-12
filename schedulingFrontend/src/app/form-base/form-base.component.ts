bfc4eeb566bf269031f8c438fc54d79487dc5c91
Zach S <zsiegel92@gmail.com>
2019-03-12 03:38:41 -0700

    component nav stuff

--- schedulingFrontend/src/app/form-base/form-base.component.ts
+++ schedulingFrontend/src/app/form-base/form-base.component.ts
@@ -1,26 +1,48 @@
-import { Component, OnInit, AfterViewInit, QueryList, Input, ViewChild, ViewChildren } from '@angular/core';
+import { Component, OnInit, AfterContentInit, QueryList, Input, ViewChild, ViewChildren, ContentChildren, ContentChild,ElementRef} from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../data.service';
 // import { StudentFormService } from '../student-form.service';
 import { MessageService } from '../message.service';
-
+import { FormService } from '../form.service';
 
 @Component({
   selector: 'app-form-base',
   templateUrl: './form-base.component.html',
   styleUrls: ['./form-base.component.css']
 })
-export class FormBaseComponent implements AfterViewInit {
-	@Input() topText: string;
-	@Input() contactEmail: string;
-	@Input() formService !: any;
-	@ViewChildren('page') pages !: QueryList<any>;
+export class FormBaseComponent implements AfterContentInit {
+	@Input('top-text') topText: string;
+	@Input('contact-email') contactEmail: string;
+	@Input('form-service') formService !: FormService;
+	@ContentChildren('page') pages !: QueryList<any>;
+	comps: any[] = [];
 	numSteps: number = 0;
 	step: number = 0;
+	styles: String[] = [];
 
+
+	removeToken(string: String,token: String){
+		var oldList: String[] = string.split(" ");
+		var newList: String[] =[];
+		for (var i=0; i < oldList.length; i++){
+			if (oldList[i] != token) {
+				newList.push(oldList[i]);
+			}
+		}
+		return newList.join(" ");
+	}
 	move(steps:number):void{
 		// this.comp.syncForm();
 		if ((this.step + steps > -1) && (this.step + steps < this.numSteps)){
+			var p1 = this.comps[this.step].nativeElement.className;
+			let p2 = this.comps[this.step + steps].nativeElement.className;
+
+			p1 = p1 + ' hidden';
+			p2 = this.removeToken(p2,'hidden');
+
+			this.comps[this.step].nativeElement.className = p1;
+			this.comps[this.step + steps].nativeElement.className= p2;
+
 			this.step = this.step + steps;
 		}
 		// this.setcomp();
@@ -42,13 +64,19 @@ export class FormBaseComponent implements AfterViewInit {
   	this.contactEmail='';
   	this.topText='';
   }
-  ngAfterViewInit() {
-  	setTimeout(() => {
+  ngAfterContentInit() {
+  	// setTimeout(() => {
   		this.pages.map((p) => {
+  			this.comps.push(p);
+  			p.nativeElement.className = 'hidden';
+  			// this.styles.push('hidden');
+  			// p.ngClass = styles[this.numSteps];
   			this.numSteps++;
   			return this.numSteps-1;
   		});
-  	});
+  	// });
+  	this.comps[0].nativeElement.className = this.removeToken(this.comps[0].nativeElement.className,'hidden');
+
   }
 
 
--- schedulingFrontend/src/app/student-form/student-form.component.ts
+++ schedulingFrontend/src/app/student-form/student-form.component.ts
@@ -5,6 +5,7 @@ import { StudentFormService } from '../student-form.service';
 import { MessageService } from '../message.service';
 // import { StudentForm1Component } from '../student-form-1/student-form-1.component';
 import { Student } from '../student';
+import { FormBaseComponent } from '../form-base/form-base.component';
 
 @Component({
   selector: 'app-student-form',
@@ -12,53 +13,53 @@ import { Student } from '../student';
   styleUrls: ['./student-form.component.css']
 })
 export class StudentFormComponent implements AfterViewInit {
-	@ViewChildren('page') pages !: QueryList<any>;
+	@ViewChild(FormBaseComponent) f;
 	// comps: any[];
-	numSteps: number = 0;
-	step: number = 0;
+	// numSteps: number = 0;
+	// step: number = 0;
 	// public comp;
 	student: Student;
 
 
 
-	move(steps:number):void{
-		// this.comp.syncForm();
-		if ((this.step + steps > -1) && (this.step + steps < this.numSteps)){
-			this.step = this.step + steps;
-		}
-		// this.setcomp();
-	}
+	// move(steps:number):void{
+	// 	// this.comp.syncForm();
+	// 	if ((this.step + steps > -1) && (this.step + steps < this.numSteps)){
+	// 		this.step = this.step + steps;
+	// 	}
+	// 	// this.setcomp();
+	// }
 
-	setcomp(){
-		// this.comp = this.comps[this.step];
-		// this.comp.prepForm();
-	}
+	// setcomp(){
+	// 	// this.comp = this.comps[this.step];
+	// 	// this.comp.prepForm();
+	// }
 
-	submit(){
-		// this.comp.syncForm();
-		this.studentFormService.submit();
-		this.router.navigate(['/submitted']);
-	}
+	// submit(){
+	// 	// this.comp.syncForm();
+	// 	this.studentFormService.submit();
+	// 	this.router.navigate(['/submitted']);
+	// }
 
-	invalidform(){
-		return false
-	}
+	// invalidform(){
+	// 	return false
+	// }
 
-	isHidden(p: any){
-		return false;
-	}
+	// isHidden(p: any){
+	// 	return false;
+	// }
 
   constructor(private dataService: DataService,private studentFormService: StudentFormService,private router: Router, private messageService: MessageService) {
   	this.student = this.studentFormService.student;
   }
 
   ngAfterViewInit() {
-  	setTimeout(() => {
-  		this.pages.map((p) => {
-  			this.numSteps++;
-  			return this.numSteps-1;
-  		});
-  	});
+  	// setTimeout(() => {
+  	// 	this.pages.map((p) => {
+  	// 		this.numSteps++;
+  	// 		return this.numSteps-1;
+  	// 	});
+  	// });
   }
 
 }
